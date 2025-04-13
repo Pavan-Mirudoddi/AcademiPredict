b308737d8933b7f4ee7ab2cd2cf572f74be49e9b
PavanMirudoddi <pavanmirudoddi46@gmail.com>
2025-04-13 23:21:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/AcademiPredict">
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
